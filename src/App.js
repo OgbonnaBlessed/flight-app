a57b60b9a1efb1d0bfcf1786d92b95eca53ee08a
Ogbonna Blessed <blessedlyrics11@gmail.com>
2024-08-12 15:35:58 +0100

    Correcting styles.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Header from "./Components/Header"
 import Stays from "./Pages/Stays"
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Stays />
-      <Footer />
     </div>
   );
 }
